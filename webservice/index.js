e92b14d0300379fb7765905c1b9978188ac4b26b
Nitin Punjabi <nitin@patternhatch.com>
2016-01-30 19:34:27 -0500

    Translate protobuf byte array in node server

--- webservice/index.js
+++ webservice/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var request = require('request');
+var protoBuf = require("protobufjs");
+
+var builder = protoBuf.loadProtoFile('../src/main/resources/instrument.proto');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
-
-app.get('/', function(req, res) {
-  res.send('I\'m the middleman!');
-});
 
 app.get('/protobuf', function(req, res) {
   request.get('http://localhost:8080/protobuf', function(error, response, body) {
@@ -15,7 +14,13 @@ app.get('/protobuf', function(req, res) {
       console.log(error);
       res.send(error);
     } else {
-      res.send(body);
+      var bytemsg = JSON.parse(body).bytes;
+      var Instrument = builder.build('instrument');
+      var translated = Instrument.Instrument.decode(bytemsg);
+      res.send({
+        original: bytemsg,
+        translated: translated
+      });
     }
   });
 });
